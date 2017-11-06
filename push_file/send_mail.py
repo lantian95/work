e09fa722fa27f8b79a4a6872b8b3b93c22acee78
lantian95 <huangruyan.huang@gmail.com>
2017-11-06 15:04:49 +0800

    send mail

---
+++ push_file/send_mail.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import sys
+import smtplib
+from email.header import Header
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+class SendMail(object):
+    def send_mail(days):
+        sender = '**'
+        receiver = '**'
+        subject = 'pushing xinlan log error'
+        smtpserver = '**'
+        username = '**'
+        password = '**'
+        message = 'pushing xinlan log has error on '+days
+        msg = MIMEText(message, 'plain', 'utf-8')
+        msg['Subject'] = Header(subject, 'utf-8')
+        msg['From'] = sender
+        smtp = smtplib.SMTP()
+        #smtp.set_debuglevel(1)
+        smtp.connect(smtpserver)
+        smtp.login(username, password)
+        smtp.sendmail(sender, receiver, msg.as_string())
+        smtp.quit()
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        _, days = tuple(sys.argv)
+    else:
+        print("param error")
+    SendMail().send_mail(days)
