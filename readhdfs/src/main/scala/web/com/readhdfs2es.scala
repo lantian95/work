ee18b3b93fc4bc46313376670e69f72e214de753
huangruyan <1219064693@qq.com>
2018-03-26 15:25:37 +0800

    Update readhdfs2es.scala

--- readhdfs/src/main/scala/web/com/readhdfs2es.scala
+++ readhdfs/src/main/scala/web/com/readhdfs2es.scala
@@ -71,7 +71,7 @@ object readhdfs2es {
       println()
     }
 
-    val line1 = "112.12.123.223 131.365 - [23/Jun/2017:00:00:08 +0800] \"GET http://kw36.videocc.net/pts/05714ecace_j48bsrqd/5/05714ecace62bd8b26be8ea95c2fdde5_3_255_312.pts?pid=1498144236668X1414972 HTTP/1.1\" 200 1512272 \"http:\n//www.iguxuan.com/index.php/tnew/spshow/id/13752.html\" \"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.104 Safari/537.36 Core/1.53.3051.400 QQBrowser/9.6.11301.400\" \nFCACHE_HIT_DISK 1.012 0.000 - - - - 0.000 131.365 131.365 1 - - 112.12.123.223 \"DISK HIT from 117.131.204.203, Configured MISS from 117.131.204.204, DISK HIT from 223.94.95.138\" 1511792 223.94.95.138"
+    val line1 = " "
     val a = readhdfs2es(line1)
     printLine(a)
 
